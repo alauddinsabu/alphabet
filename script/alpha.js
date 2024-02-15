94d9a82985e8085d12830501deb35ac5d9e8a5b7
alauddinsabu <alauddinmu76@Gmail.com>
2024-02-15 22:45:02 +0600

    fourth commit

---
+++ script/alpha.js
@@ -0,0 +1,21 @@
+//function play(){
+// const homescreen = document.getElementById('home-screen');
+// homescreen.classList.add('hidden');
+ //const palyground = document.getElementById('play-ground');
+ //palyground.classList.remove('hidden');
+//}
+
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    console.log(alphabet);
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+}
+
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
---
+++ script/utility.js
@@ -0,0 +1,21 @@
+function hideElementById(elementId){
+    const elements = document.getElementById(elementId);
+    elements.classList.add('hidden');
+}
+
+function showElementById(elementId){
+    const elements = document.getElementById(elementId);
+    elements.classList.remove('hidden');
+}
+function getARandomAlphabet(){
+    const alphabetstring = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabet = alphabetstring.split('');
+    console.log(alphabet);
+
+    const randomNumber = Math.random() *25;
+    const index = Math.round(randomNumber);
+   
+ const alphabets = alphabet[index];
+    
+    return alphabets
+}
